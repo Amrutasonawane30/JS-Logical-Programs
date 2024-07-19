0323bc9811688f38853cb43b1c15fd323b3fb1e0
Amrutasonawane30 <160743814+Amrutasonawane30@users.noreply.github.com>
2024-07-19 14:52:53 +0530

    Create 01_Array.js
    Javascript program to print number of element present in array

---
+++ 01_Array.js
@@ -0,0 +1,7 @@
+//Javascript program to print number of element present in array
+
+function arrayLength(arr){
+console.log(`Number of elements present in array `,arr.length)
+}
+let array = [1,2,3,4,5,66];
+arrayLength(array);
